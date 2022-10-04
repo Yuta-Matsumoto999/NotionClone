2656b664fe5c508b876ab63629691f8b35832a5c
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-05 02:32:39 +0900

    ユーザー新規登録API作成

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+const User = require("./src/v1/models/user")
 const app = express();
 const PORT = 5050;
 require("dotenv").config();
+
+app.use(express.json());
 
 // DB接続
 try {
@@ -13,6 +18,32 @@ try {
 }
 
 // ユーザー新規登録用API
+app.post("/register", async (req, res) => {
+    // パスワードの受け取り
+    const password = req.body.password;
+
+    try {
+        // パスワードの暗号化
+        req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+
+        // ユーザーの新規登録
+        const user = await User.create(req.body);
+
+        // JWTの発行
+        const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+            expiresIn: "24h",
+        });
+
+        return res.status(200).json({ user, token });
+    } catch (error) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
 
 
 // ユーザーログイン用API
