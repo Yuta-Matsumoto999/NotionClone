7f8dbf14dbdb362ef19ef834886b3a6c07e9f7d1
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-16 09:27:17 +0900

    Material UI インストール

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import AccessAlarmsOutlinedIcon from '@mui/icons-material/AccessAlarmsOutlined';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div>Login <AccessAlarmsOutlinedIcon /></div>;
 }
 
 export default Login;
