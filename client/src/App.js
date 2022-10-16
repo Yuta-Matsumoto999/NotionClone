c85acc18361edb6c24e6f8a497ec46a1c5997930
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-16 09:19:31 +0900

    認証用のコンポーネントを用意

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Login from './pages/Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from './components/layout/AuthLayout';
+import Register from './pages/Register';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
+  return ( 
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
