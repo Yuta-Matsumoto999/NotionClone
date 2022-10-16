b68395785ac0e874fc9bb245a8fb80895a6a8085
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-16 15:37:56 +0900

    axiosインスタンス作成、Http通信での前処理を追加

---
+++ client/src/api/axiosClient.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5050/api/v1";
+
+const getToken = () => {
+    localStorage.getItem("token");
+}
+
+const axiosClient =axios.create({
+    baseURL: BASE_URL
+});
+
+// API request時の前処理を行う
+axiosClient.interceptors.request.use(async (config) =>  {
+    return {
+        config,
+        headers: {
+            "Content-type": "application/json",
+            authorization: `Bearer ${getToken()}`,  // request headerにJWTを付けてサーバーに渡す
+        },
+    };
+});
+
+// API Response時の前処理を行う
+axiosClient.interceptors.response.use(async (response) => {
+    return response;
+    }, 
+    (err) => {
+        throw err.response;
+    }
+);
+
+export default axiosClient;
