42e78f4a07f79cc26bcd18c7c26df50c61617bfe
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-18 14:18:13 +0900

    クライアントがJWTを持っているかの検証を実装

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,10 +1,32 @@
 import { Box } from '@mui/material';
 import { Container, flexbox } from '@mui/system';
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from '../../utils/authUtils';
 
 const AuthLayout = () => {
+
+    const navigate = useNavigate();
+
+    // ページ遷移が発生する度に更新される処理を記述
+    useEffect(() => {
+        // JWTを持っているか確認する
+        const checkAuth = async () => {
+            // 認証チェック
+            const isAuth = await authUtils.isAuthenticated();
+
+            console.log(isAuth);
+
+            // 認証済みであればHomeへリダレクト
+            if(isAuth) {
+                navigate("/");
+            }
+        };
+        checkAuth();
+    }, [navigate]);
+
     return (
         <div>
             <Container component="main" maxWidth="xs">
