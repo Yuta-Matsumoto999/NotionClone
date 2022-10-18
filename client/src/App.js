81b71dee3c56f7e9f9ecdabe6e7139cfa6ff2664
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-18 20:32:12 +0900

    App本体のルーティング設定追加

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Login from './pages/Login';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthLayout from './components/layout/AuthLayout';
+import AppLayout from './components/layout/AppLayout';
 import Register from './pages/Register';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from '@mui/material';
 import { blue } from "@mui/material/colors";
+import Home from './pages/Home';
 
 function App() {
 
@@ -21,6 +23,10 @@ function App() {
           <Route path="/" element={<AuthLayout />}>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
+          </Route>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />}/>
+            <Route path="memo" element={<Home />}/>
           </Route>
         </Routes>
       </BrowserRouter>
