b521ae56b19a3988375c6e3cad10f627370af805
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-28 12:00:35 +0900

    memo詳細画面を追加

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from '@mui/material';
 import { blue } from "@mui/material/colors";
 import Home from './pages/Home';
+import Memo from "./pages/Memo";
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
           <Route path="/" element={<AppLayout />}>
             <Route index element={<Home />}/>
             <Route path="memo" element={<Home />}/>
+            <Route path="memo/:memoId" element={<Memo />}/>
           </Route>
         </Routes>
       </BrowserRouter>
