6859120036571879a0b2d236fa738df0ba578374
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-28 17:57:39 +0900

    メモクリック時のハイライト機能追加

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -2,17 +2,18 @@ import { Drawer, IconButton, List, ListItemButton, Typography } from '@mui/mater
 import { Box, width } from '@mui/system';
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import assets from '../../assets/index';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import memoApi from '../../api/memoApi';
 import { setMemo } from '../../redux/features/memoSlice';
 
 const Sidebar = () => {
-
+    const [activeIndex, setActiveIndex] = useState(0);
     const dispatch  = useDispatch();
     const navigate = useNavigate();
+    const { memoId } = useParams();
     const user = useSelector((state) => state.user.value);
     const memos = useSelector((state) => state.memo.value);
 
@@ -32,6 +33,11 @@ const Sidebar = () => {
         };
         getMemos();
     }, []);
+
+    useEffect(() => {
+        const activeIndex = memos.findIndex((e) => e._id === memoId);
+        setActiveIndex(activeIndex);
+    }, [navigate]);
 
     return (
         <Drawer container={window.document.body} variant="permanent" open={true} sx={{width: 250, height: "100vh"}}>
@@ -71,6 +77,7 @@ const Sidebar = () => {
                         sx={{pl: "20px"}} 
                         component={Link} to={`/memo/${item._id}`}
                         key={item._id}
+                        selected={index === activeIndex}
                     >
                         <Typography>
                             {item.icon} {item.title}
