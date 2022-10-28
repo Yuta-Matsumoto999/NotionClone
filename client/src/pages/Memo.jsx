f365c4572b0bf58334524e98ba6a3a1b50bfbba1
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-28 18:25:03 +0900

    Memoコンポーネント整形

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,8 +1,38 @@
+import { Box, IconButton, TextField } from '@mui/material'
 import React from 'react'
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 function Memo() {
     return (
-        <div>Memo</div>
+        <>
+        <Box sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%"
+        }}>
+            <IconButton>
+                <StarBorderOutlinedIcon />
+            </IconButton>
+            <IconButton variant="outlined" color="error">
+                <DeleteOutlinedIcon />
+            </IconButton>
+        </Box>
+        <Box sx={{padding: "10px 50px"}}>
+            <TextField placeholder="無題" variant="outlined" fullWidth sx={{
+                ".MuiOutlinedInput-input": { padding: 0 },
+                ".MuiOutlinedInput-notchedOutline" : { border: "none" },
+                ".MuiOutlinedInput-root" : { fontSize: "2rem", fontWeight: 700 },
+            }}
+            />
+            <TextField placeholder="追加" variant="outlined" fullWidth sx={{
+                ".MuiOutlinedInput-input": { padding: 0 },
+                ".MuiOutlinedInput-notchedOutline" : { border: "none" },
+                ".MuiOutlinedInput-root" : { fontSize: "1rem" }
+            }}
+            />
+        </Box>
+        </>
     )
 }
 
