ebe005365f155d6df68fcf6cb4cad808be60f0fd
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-28 19:06:03 +0900

    個別に取得したメモ詳細を表示

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,9 +1,29 @@
 import { Box, IconButton, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useParams } from 'react-router-dom';
+import memoApi from '../api/memoApi';
+
 
 function Memo() {
+    const { memoId } = useParams();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        const getMemo = async () => {
+            try {
+                const memo = await memoApi.getOne(memoId);
+                setTitle(memo.title);
+                setDescription(memo.description);
+            } catch (err) {
+                alert(err);
+            }
+        }
+        getMemo();
+    }, [memoId]);
+
     return (
         <>
         <Box sx={{
@@ -24,12 +44,14 @@ function Memo() {
                 ".MuiOutlinedInput-notchedOutline" : { border: "none" },
                 ".MuiOutlinedInput-root" : { fontSize: "2rem", fontWeight: 700 },
             }}
+            value={title}
             />
             <TextField placeholder="追加" variant="outlined" fullWidth sx={{
                 ".MuiOutlinedInput-input": { padding: 0 },
                 ".MuiOutlinedInput-notchedOutline" : { border: "none" },
                 ".MuiOutlinedInput-root" : { fontSize: "1rem" }
             }}
+            value={description}
             />
         </Box>
         </>
