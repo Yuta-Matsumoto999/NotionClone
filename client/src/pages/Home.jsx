6b1fa5499f90707c33c85df51fb4974377379681
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-29 02:36:03 +0900

    新規作成したメモをグローバルに管理するstateへ追加

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,17 +4,24 @@ import { LoadingButton } from '@mui/lab';
 import { useState } from 'react';
 import memoApi from "../api/memoApi";
 import { useNavigate } from 'react-router-dom';
-
+import { useSelector, useDispatch } from "react-redux";
+import { setMemo } from '../redux/features/memoSlice';
 
 const Home = () => {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
+    const memos = useSelector((state) => state.memo.value);
+    const dispatch  = useDispatch();
 
     const createMemo = async () => {
         try {
             setLoading(true);
             const res = await memoApi.create();
-            console.log(res);
+
+            // 作成したメモを既存のメモへ追加
+            const newMemos = [res, ...memos];
+            dispatch(setMemo(newMemos));
+
             navigate(`/memo/${res._id}`);
         } catch (err) {
             alert(err);
