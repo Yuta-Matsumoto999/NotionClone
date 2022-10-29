69248e42c927d19ff7ccfcb8054fa3cce8af255b
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-29 18:58:18 +0900

    絵文字pickerで選択した絵文字を取得する

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -6,16 +6,36 @@ import Picker from '@emoji-mart/react';
 
 const EmojiPicker = (props) => {
     const [selectedEmoji, setSelectedEmoji] = useState();
+    const [isShowPicker, setIsShowPicker] = useState(false);
 
     useEffect(() => {
         setSelectedEmoji(props.icon);
     }, [props.icon]);
 
+    const showPicker = () => {
+        setIsShowPicker(!isShowPicker);
+    }
+
+    const selectEmoji = (e) => {
+        // 絵文字固有のunifiedコードを取得
+        // -で繋がっているコードもあるため、分割する
+        const emojiCode = e.unified.split("-");
+
+        // 取得したunifiedコードを1つずつ配列へ入れる
+        let codesArray = [];
+        emojiCode.forEach((el) => codesArray.push("0x" + el));
+
+        // unifiedコードの配列を展開し、絵文字に変換する
+        const emoji = String.fromCodePoint(...codesArray);
+
+        setIsShowPicker(false);
+    }
+
     return (
         <Box>
-            <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>{selectedEmoji}</Typography>
-            <Box sx={{display: "none"}}>
-                <Picker />
+            <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer", marginBottom: "15px" }} onClick={showPicker}>{selectedEmoji}</Typography>
+            <Box sx={{display: isShowPicker ? "block" : "none", position: "absolute", zIndex: "100"}}>
+                <Picker onEmojiSelect={selectEmoji} />
             </Box>
         </Box>
     )
