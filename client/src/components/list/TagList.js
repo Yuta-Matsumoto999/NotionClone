e06527e297bdbfd45d4e2852066c7742501a6e64
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-11-02 14:04:52 +0900

    memoのD&Dコンポーネント編集

--- client/src/components/list/TagList.js
+++ client/src/components/list/TagList.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { Box, Button, Grid, IconButton, TextField, Typography } from '@mui/material';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
-const TagList = () => {
+const TagList = (props) => {
     return (
-        <div>TagList</div>
+        <Box sx={{margin: "0 20px", textAlign: "left"}} width="25%">
+            <Droppable droppableId={props.id}>
+                {(provided, snapshot) => (
+                <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                >
+                    <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                        <Typography sx={{padding: "1px 6px", backgroundColor: props.color, borderRadius: "3px"}} fontWeight="700" fontSize="14px">{props.tagName}</Typography>
+                        <Box sx={{display: "flex"}}>
+                            <Typography><Button sx={{padding: "2px"}}><MoreHorizOutlinedIcon fontSize='small'/></Button></Typography>
+                            <Button sx={{padding: "2px"}}><AddOutlinedIcon fontSize='small' /></Button>
+                        </Box>
+                    </Box>
+                    {props.memos.map((item, index) => (
+                    <Draggable key={item.id} draggableId={item.tag} index={index}>
+                        {(provided) => (
+                            <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                            >
+                                <Box sx={{display: "flex", border: "solid 1px #eee", borderRadius: "3px", padding: "1px 3px"}}>
+                                    <Typography fontWeight="700">{item.title}</Typography>
+                                </Box>
+                            </div>
+                        )}
+                    </Draggable>
+                    ))}
+                    {provided.placeholder}
+                    {/* <button className="Add-item-btn" onClick={() => props.onAddItems(props.id)}></button> */}
+                </div>
+                )}
+            </Droppable>
+        </Box>
     )
 }
 
--- client/src/components/listGroup/TagListGroup.jsx
+++ client/src/components/listGroup/TagListGroup.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import TagList from '../list/TagList';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { useSelector, useDispatch } from "react-redux";
+import { Box } from '@mui/material';
 
-const TagListGroup = (props) => {
+const TagListGroup = () => {
+    const tags = useSelector((state) => state.tag.value);
+    console.log(tags);
+
+    const addItems = () => {
+
+    }
+
     return (
-        <>
-        <div>TagListGroup</div>
-        <TagList />
-        </>
+        <DragDropContext>
+            <Box sx={{display: "flex"}}>
+                {tags.map((value, index) => 
+                    <TagList 
+                    key={index} 
+                    id={value._id}
+                    tagName={value.name}
+                    color={value.color}
+                    memos={value.memos}
+                    AddItems={addItems}
+                    />
+                )}
+            </Box>
+        </DragDropContext>
     )
 }
 
